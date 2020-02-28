e3e28fd00850f9e5124a69d07535de5509b0a261
scRaul <61263078+scRaul@users.noreply.github.com>
2020-02-27 23:25:15 -0800

    Version2.0

---
+++ index.js
@@ -0,0 +1,34 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+
+
+
+
+function scale(){
+    let L = document.getElementById("CSS"); 
+    var w = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+    var h = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+
+    if( h > w ){ L.href="mobile.css";}
+    else{ L.href="index.css"}
+}
+scale();
+window.addEventListener("resize",scale);
